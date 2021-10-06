56aecc13296d0d7cccbb36ffc3609a6a80e1aa55
Karthik261 <chamurukarthik@gmail.com>
2021-10-06 12:51:02 +0530

    Initial Commit

---
+++ src/Sample.java
@@ -0,0 +1,7 @@
+public class Sample {
+    public static void main(String[] args)
+    {
+        System.out.println("Hi this is my program");
+        System.out.println("My first prgrm");
+    }
+}
